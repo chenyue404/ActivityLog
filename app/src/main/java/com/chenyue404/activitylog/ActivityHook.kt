f2d3f41e374b256f44144fcbf255edd5587e1a31
chenyue404 <601450225@qq.com>
2021-04-09 21:22:50 +0800

    versions

--- app/src/main/java/com/chenyue404/activitylog/ActivityHook.kt
+++ app/src/main/java/com/chenyue404/activitylog/ActivityHook.kt
@@ -1,9 +1,9 @@
 package com.chenyue404.activitylog
 
+import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.os.IBinder
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
@@ -23,11 +23,69 @@ class ActivityHook : IXposedHookLoadPackage {
             return
         }
 
-        when (Build.VERSION.SDK_INT) {
-            Build.VERSION_CODES.O_MR1 -> hook27(classLoader)
-            Build.VERSION_CODES.Q,
-            Build.VERSION_CODES.R -> hook30(classLoader)
-            else -> log("暂不支持这个版本的Android")
+        val osVersion = Build.VERSION.SDK_INT
+        log("osVersion=$osVersion")
+        if (osVersion <= 27) {
+            val activityRecordClass =
+                XposedHelpers.findClass(
+                    "com.android.server.am.ActivityRecord",
+                    classLoader
+                )
+            val iVoiceInteractionSessionClass =
+                XposedHelpers.findClass(
+                    "android.service.voice.IVoiceInteractionSession",
+                    classLoader
+                )
+            val iVoiceInteractorClass =
+                XposedHelpers.findClass(
+                    "com.android.internal.app.IVoiceInteractor",
+                    classLoader
+                )
+            val taskRecordClass =
+                XposedHelpers.findClass("com.android.server.am.TaskRecord", classLoader)
+            if (osVersion < 26) {
+                //24,25
+                XposedHelpers.findAndHookMethod(
+                    "com.android.server.am.ActivityStarter",
+                    classLoader,
+                    "startActivityUnchecked",
+                    activityRecordClass,
+                    activityRecordClass,
+                    iVoiceInteractionSessionClass,
+                    iVoiceInteractorClass,
+                    Int::class.java,
+                    Boolean::class.java,
+                    ActivityOptions::class.java,
+                    taskRecordClass,
+                    hookStartActivityUnchecked()
+                )
+            } else {
+                //26,27
+                XposedHelpers.findAndHookMethod(
+                    "com.android.server.am.ActivityStarter",
+                    classLoader,
+                    "startActivityUnchecked",
+                    activityRecordClass,
+                    activityRecordClass,
+                    iVoiceInteractionSessionClass,
+                    iVoiceInteractorClass,
+                    Int::class.java,
+                    Boolean::class.java,
+                    ActivityOptions::class.java,
+                    taskRecordClass,
+                    arrayOf(activityRecordClass)::class.java,
+                    hookStartActivityUnchecked()
+                )
+            }
+        } else if (osVersion <= 30) {
+            val className = if (osVersion == 28) {
+                "com.android.server.am.ActivityStarter"
+            } else {
+                "com.android.server.wm.ActivityStarter"
+            }
+            hookExecute(className, classLoader)
+        } else {
+            log("暂不支持这个版本的Android")
         }
     }
 
@@ -35,57 +93,9 @@ class ActivityHook : IXposedHookLoadPackage {
         XposedBridge.log("$tag-$str")
     }
 
-    private fun hook27(classLoader: ClassLoader) {
-        /**
-         * int startActivityMayWait(IApplicationThread caller, int callingUid,
-        String callingPackage, Intent intent, String resolvedType,
-        IVoiceInteractionSession voiceSession, IVoiceInteractor voiceInteractor,
-        IBinder resultTo, String resultWho, int requestCode, int startFlags,
-        ProfilerInfo profilerInfo, WaitResult outResult,
-        Configuration globalConfig, Bundle bOptions, boolean ignoreTargetSecurity, int userId,
-        TaskRecord inTask, String reason)
-         */
-
-        val iApplicationThreadClass =
-            XposedHelpers.findClass("android.app.IApplicationThread", classLoader)
-        val iVoiceInteractionSessionClass =
-            XposedHelpers.findClass("android.service.voice.IVoiceInteractionSession", classLoader)
-        val iVoiceInteractorClass =
-            XposedHelpers.findClass("com.android.internal.app.IVoiceInteractor", classLoader)
-        val profilerInfoClass =
-            XposedHelpers.findClass("android.app.ProfilerInfo", classLoader)
-        val waitResultClass =
-            XposedHelpers.findClass("android.app.WaitResult", classLoader)
-        val configurationClass =
-            XposedHelpers.findClass("android.content.res.Configuration", classLoader)
-        val taskRecordClass =
-            XposedHelpers.findClass("com.android.server.am.TaskRecord", classLoader)
-
+    private fun hookExecute(className: String, classLoader: ClassLoader) {
         XposedHelpers.findAndHookMethod(
-            "com.android.server.am.ActivityStarter", classLoader, "startActivityMayWait",
-            iApplicationThreadClass, Int::class.java,
-            String::class.java, Intent::class.java, String::class.java,
-            iVoiceInteractionSessionClass, iVoiceInteractorClass,
-            IBinder::class.java, String::class.java, Int::class.java, Int::class.java,
-            profilerInfoClass, waitResultClass,
-            configurationClass, Bundle::class.java, Boolean::class.java, Int::class.java,
-            taskRecordClass, String::class.java,
-            object : XC_MethodHook() {
-                override fun afterHookedMethod(param: MethodHookParam) {
-                    val callingPackage = param.args[2] as String
-                    val intent: Intent = param.args[3] as Intent
-                    val bundle: Bundle = param.args[14] as Bundle
-                    val str = intent.transToStr(callingPackage, bundle)
-                    log("\n$str")
-                }
-            }
-        )
-    }
-
-
-    private fun hook30(classLoader: ClassLoader) {
-        XposedHelpers.findAndHookMethod(
-            "com.android.server.wm.ActivityStarter", classLoader, "execute",
+            className, classLoader, "execute",
             object : XC_MethodHook() {
                 override fun beforeHookedMethod(param: MethodHookParam) {
                     val request = XposedHelpers.getObjectField(param.thisObject, "mRequest")
@@ -112,5 +122,22 @@ class ActivityHook : IXposedHookLoadPackage {
                 }
             }
         )
+    }
+
+    private fun hookStartActivityUnchecked() = object : XC_MethodHook() {
+        override fun afterHookedMethod(param: MethodHookParam) {
+            log("hookStartActivityUnchecked")
+            val activityRecord = param.args[0]
+            val activityOptions = param.args[6] as ActivityOptions
+            val bundle = activityOptions.toBundle()
+            val callingPackage =
+                XposedHelpers.getObjectField(activityRecord, "launchedFromPackage")?.let {
+                    it as String
+                } ?: ""
+            val intent = XposedHelpers.getObjectField(activityRecord, "intent")?.let {
+                it as Intent
+            } ?: return
+            log("\n${intent.transToStr(callingPackage, bundle)}")
+        }
     }
 }
